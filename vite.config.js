564c1ea558ecc03ffd163056bd72a958ac142477
alperenkucukozkan <alperenkucukozkan@gmail.com>
2025-10-07 12:24:51 +0300

    finale

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,46 @@
 import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import path from 'path';
 
-export default defineConfig({
-  base: '/goit-js-hw-09/',
-  server: {
-    open: false,
-    port: 5173,
-  },
-  define: {
-    'global': {},
-  }
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    base: './',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      sourcemap: true,
+      rollupOptions: {
+        input: Object.fromEntries(
+          glob.sync('./src/public/*.html').map(file => {
+            const name = path.basename(file);
+            return [name, file];
+          })
+        ),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: '[name].js',
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['src/public/**/*.html']),
+    ],
+  };
 });
